ad54219c422cb9b3acfc585d53c1186dcb660199
Lauro Moraes <lauromoraes.poa.rs@gmail.com>
2018-10-24 17:19:44 -0300

    implement wildcard #3

--- Emitter.js
+++ Emitter.js
@@ -1,41 +1,29 @@
 /**
  * @license The MIT License (MIT)             - [https://github.com/subversivo58/Emitter/blob/master/LICENSE]
- * @copyright Copyright (c) 2017 Lauro Moraes - [https://github.com/subversivo58]
+ * @copyright Copyright (c) 2018 Lauro Moraes - [https://github.com/subversivo58]
  * @version 0.1.0 [development stage]         - [https://github.com/subversivo58/Emitter/blob/master/VERSIONING.md]
  */
- 
- class Emitter extends EventTarget {
+
+export class Emitter extends EventTarget {
     constructor() {
         super()
-        // store listeners
-        this.listeners = {}
+        // store listeners (by callback)
+        this.listeners = {
+            '*': [] // pre alocate for all (wildcard)
+        }
     }
-    
-    /**
-     * Initialize listener(s)
-     * @param {String}     e - event target name
-     * @param {Function}  cb - callback to receive `CustomEvent` object
-     * @param {Boolean} once - indicator trigger only once
-     */
     on(e, cb, once = false) {
         // store one-by-one registered listeners
         !this.listeners[e] ? this.listeners[e] = [cb] : this.listeners[e].push(cb)
         // check `.once()` ... callback `CustomEvent`
         once ? this.addEventListener(e, cb, { once: true }) : this.addEventListener(e, cb)
     }
-    
-    /**
-     * Disable listener(s)
-     * @param {String}    e - event target name
-     * @param {Function} Fn - callback function reference (default false)
-     */
     off(e, Fn = false) {
         if ( this.listeners[e] ) {
             // remove listener (include ".once()")
             let removeListener = target => {
                 this.removeEventListener(e, target)
             }
-
             // use `.filter()` to remove expecific event(s) associated to this callback
             const filter = () => {
                 this.listeners[e] = this.listeners[e].filter(val => {
@@ -43,43 +31,28 @@
                 })
                 // check number of listeners for this target ... remove target if empty
                 if ( this.listeners[e].length === 0 ) {
-                    delete this.listeners[e]
+                    e !== '*' ? delete this.listeners[e] : null
                 }
             }
-
             // use `.forEach()` to iterate all listeners for this target
             const iterate = () => {
-                this.listeners[e].forEach((val, index, array) => {
-                    removeListener(val)
-                    // on end "loop" remove all listeners references for this target (by target object)
-                    if ( index === array.length -1 ) {
-                        delete this.listeners[e]
-                    }
-                })
+                let len = this.listeners[e].length
+                while (len--) {
+                    removeListener(this.listeners[e][len])
+                }
+                // remove all listeners references (callbacks) for this target (by target object)
+                e !== '*' ? delete this.listeners[e] : this.listeners[e] = []
             }
-
             Fn && typeof Fn === 'function' ? filter() : iterate()
         }
     }
-    
-    /**
-     * Fire a event - dispatched from `CustomEvent`
-     * @param {String}              e - event target name
-     * @param {String|Object|Array} d - data
-     */
     emit(e, d) {
-        this.dispatchEvent(new CustomEvent(e, {detail: d}))
+         if ( this.listeners['*'].length > 0 ) {
+             this.dispatchEvent(new CustomEvent('*', {detail: d}))
+         }
+         this.dispatchEvent(new CustomEvent(e, {detail: d}))
     }
-    
-    /**
-     * Fire a once event - uses `.on()`
-     * @param {String}    e - event target name
-     * @param {Function} cb - callback to receive `CustomEvent` object
-     */
     once(e, cb) {
         this.on(e, cb, true)
     }
 }
-
-//
-export default Emitter
